f6810db7892efa33a78e389d24cec7204fe8efa7
Robert Dzikowski <rdzikowski@live.com>
2023-07-14 23:29:06 +0200

    modified:   README.md modified:   cypress/e2e/login_locked_out_user.cy.js new file:   cypress/e2e/swag_labs_tests.js

--- cypress/e2e/swag_labs_tests.js
+++ cypress/e2e/swag_labs_tests.js
@@ -1,12 +1,16 @@
 describe('Swag Labs Tests.', () => {
-  it('I should be able to login as locked_out_user', () => {
+  beforeEach(() => {
     cy.visit('https://www.saucedemo.com/')
     cy.get('.login_logo').should('have.text', 'Swag Labs')
-    cy.get("#user-name").type('locked_out_user')
+    cy.get("#user-name").type('standard_user')
     cy.get("#password").type('secret_sauce')
     cy.get("#login-button").click()
-    // debugger;
+
+    cy.url().should('contain', 'www.saucedemo.com/inventory.html')
     cy.get('.title').should('be.visible')
     cy.get('.title').should('have.text', 'Products')
+  })
+
+  it('Sort by products should work correctly', () => {
   })
 })
